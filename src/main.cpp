70e99869152cc0fc71b4b7b9c1ba6ab2969cefbe
Andrew Kraken <took77git@list.ru>
2022-06-13 18:05:54 +0300

    Get_Started_End

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <lvgl.h>
 #include "lv_conf.h"
 #include <WT32settings.h>
-#include <Buttons_demo.h>
+//#include <Buttons_demo.h>
+//#include<Get_Started_1.h>
+#include<Get_Started_3.h>
 // место для инклуда нового Демо.h
 
 void setup() {
   Serial.begin(115200); /* prepare for possible serial debug */
   DisplayInit();
   // вызов функции нового демо из Демо.h
-  lv_button_demo();
+  //lv_button_demo();
+  //lv_example_get_started_1();
+  lv_example_get_started_3();
 }
 
 void loop() {
