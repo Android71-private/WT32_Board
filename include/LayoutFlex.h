e1d83cbbacbab4a0b54a58b21cdb8c02d7b44cbe
Andrew Kraken <took77git@list.ru>
2022-06-13 19:31:36 +0300

    Layout

---
+++ include/LayoutFlex.h
@@ -0,0 +1,6 @@
+#include <Layout_Flex_1.h>
+#include <Layout_Flex_2.h>
+#include <Layout_Flex_3.h>
+#include <Layout_Flex_4.h>
+#include <Layout_Flex_5.h>
+#include <Layout_Flex_6.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
+#include <Arduino.h>
 #include <lvgl.h>
 #include "lv_conf.h"
 #include <WT32settings.h>
 //#include <Buttons_demo.h>
-//#include<Get_Started_1.h>
-#include<Get_Started_3.h>
+
+//#include <Get_Started_1.h>
+//#include <Get_Started_2.h>
+//#include <Get_Started_3.h>
+
+#include <Get_Started.h>
+#include <LayoutFlex.h>
 // место для инклуда нового Демо.h
 
 void setup() {
   Serial.begin(115200); /* prepare for possible serial debug */
   DisplayInit();
   // вызов функции нового демо из Демо.h
+  
   //lv_button_demo();
-  //lv_example_get_started_1();
-  lv_example_get_started_3();
+
+  lv_example_get_started_1();
+  //lv_example_get_started_2();
+  //lv_example_get_started_3();
+
+  //lv_example_flex_1();
+  //lv_example_flex_2();
+  //lv_example_flex_3();
+  //lv_example_flex_4();
+  //lv_example_flex_5();
+  //lv_example_flex_6();
 }
 
 void loop() {
